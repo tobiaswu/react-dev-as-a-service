d218a60c44a5411de86318f3ef78667a3c2d0282
Tobias Wupperfeld <tw@TobiasMacBook.local>
2023-07-02 12:36:03 +0200

    feat: initial commit

---
+++ src/components/Footer/Footer.styled.ts
@@ -0,0 +1,18 @@
+'use client'
+
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #252525;
+  padding: 1rem;
+`;
+
+export const FooterText = styled.p`
+  color: #c6c5b9;
+  font-size: 1rem;
+`;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { FooterContainer, FooterText } from "./Footer.styled"
+
+export const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterText>Copyright © 2023 TW Software Solutions LLC. All Rights Reserved.</FooterText>
+        </FooterContainer>
+    )
+}
